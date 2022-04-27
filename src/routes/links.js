7b0cd9e8af3c9e02105613efde5dff19c50d33b8
Charly Aguirre <charagmz@gmail.com>
2022-04-27 04:26:57 -0500

    Adds links

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,6 +5,18 @@ const pool = require('../database');
 
 router.get('/add', (req, res) => {
     res.render('links/add');
-})
+});
+
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    console.log(newLink);
+    res.send('recibido');
+});
 
 module.exports = router;
